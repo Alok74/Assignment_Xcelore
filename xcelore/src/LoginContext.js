2debb2ebb8f748167d083c07ba76d92da057e9c2
Alok Verma <94034794+Alok74@users.noreply.github.com>
2024-07-08 02:19:44 +0530

    Update LoginContext.js

--- xcelore/src/LoginContext.js
+++ xcelore/src/LoginContext.js
@@ -7,32 +7,23 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(""); // Initialize user state
- const [isAuthenticated,setIsAuthenticated]=useState(false)
-  // Check if there's a token in cookies on initial load
+ const [isAuthenticated,setIsAuthenticated]=useState(false);
   const tokenFromCookie = Cookies.get('token');
   if (tokenFromCookie && !user) {
-    // Set user based on token or perform any token validation logic
     setUser({ token: tokenFromCookie });
   }
 
   const login = (userData) => {
     setIsAuthenticated(!isAuthenticated)
     setUser(userData.user);
-    Cookies.set('token', userData.token, { expires: 7 }); // Token expires in 7 days
+    Cookies.set('token', userData.token, { expires: 7 }); 
   };
 
   const logout = () => {
-    // Clear user state on logout
     setUser(null);
-    
-    // Remove token from cookies
     Cookies.remove('token');
     setIsAuthenticated(!isAuthenticated)
   };
-
-  // const isAuthenticated = () => {
-  //   return !!user; // Check if user is authenticated
-  // };
 
   return (
     <AuthContext.Provider value={{ user, login, logout, isAuthenticated }}>
