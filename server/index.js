49c7c3a1db18b5c3b2151fcbd4b8665e18004388
Alok Verma <94034794+Alok74@users.noreply.github.com>
2024-07-08 02:13:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,32 +3,20 @@ const todoRoute= require('./routes');
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
-//load config from env file
-//require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 app.use(cors());
-//middleware to parse json request body
 app.use(express.json());
-
-//import routes for TODO API
 //index.jsconst todoRoutes = require("./routes/todos");
 
-//mount the todo API routes
 app.use("/api/v1", todoRoute);
-
-
-//start server
 app.listen(PORT, () => {
     console.log(`Server started successfully at ${PORT}`);
 })
 
-//connect to the database
 const dbConnect = require("./database");
 //const { deleteTodo } = require('./controller');
 dbConnect();
 
-//default Route
 app.get("/", (req,res) => {
-    res.send(`<h1> This is HOMEPAGE baby</h1>`);
+    res.send(`<h1> This is HOMEPAGE</h1>`);
 })
