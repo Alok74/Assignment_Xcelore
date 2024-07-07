e071019ad383cdd5a84a16412b68ff365482c64d
Alok Verma <94034794+Alok74@users.noreply.github.com>
2024-07-08 02:16:17 +0530

    Update LoginForm.js

--- xcelore/src/components/LoginForm.js
+++ xcelore/src/components/LoginForm.js
@@ -26,8 +26,6 @@ const LoginForm = () => {
     } catch (error) {
       console.error('Login error:', error);
       if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
         console.error('Error response data:', error.response.data);
         console.error('Error response status:', error.response.status);
         console.error('Error response headers:', error.response.headers);
@@ -35,7 +33,6 @@ const LoginForm = () => {
         // The request was made but no response was received
         console.error('Error request:', error.request);
       } else {
-        // Something happened in setting up the request that triggered an Error
         console.error('Error message:', error.message);
       }
     }
