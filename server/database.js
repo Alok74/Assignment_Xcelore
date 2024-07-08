7f9556fcc3f10586274b3a8abc8dff8e3abe3499
Alok Verma <94034794+Alok74@users.noreply.github.com>
2024-07-08 10:26:32 +0530

    Update database.js

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbConnect = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/testing", {
+    mongoose.connect("mongodb://alokverma0122:alok74@ac-beu7tla-shard-00-00.jtkqykg.mongodb.net:27017,ac-beu7tla-shard-00-01.jtkqykg.mongodb.net:27017,ac-beu7tla-shard-00-02.jtkqykg.mongodb.net:27017/testing?ssl=true&replicaSet=atlas-i3qobz-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0", {
         useNewUrlParser:true,
         useUnifiedTopology: true,
     })
